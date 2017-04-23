11e63c978121264ddafa48a791fb531883e68bd8
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-04-24 00:36:46 +0530

    updated demo tab components

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/clusterEvaluation')
+def clusterEvaluation():
+    return render_template('clusterEvaluation.html')
+
+@app.route('/machineLearning')
+def machineLearning():
+    return render_template('mlRecommender.html')
 
 if __name__=="__main__":
     app.run(debug=True)
